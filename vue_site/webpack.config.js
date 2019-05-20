f69974eb534972ed373acfc88520686e7df1b83c
hhwkns <hhwkns@gmail.com>
2019-05-19 20:59:58 -0700

    vue pages test

--- vue_site/webpack.config.js
+++ vue_site/webpack.config.js
@@ -5,8 +5,10 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'build.js',
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/'
   },
   module: {
     rules: [
